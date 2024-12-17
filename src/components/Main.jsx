c5ed6b11b9027783b94d9eed9e4416beb58c183a
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-12-17 14:33:10 +0100

    creazione post coi bottoni

---
+++ src/components/Main.jsx
@@ -0,0 +1,67 @@
+function Main() {
+    return (
+        <>
+            <main>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "36rem" }}>
+                    <img src="placeholders.dev-1280x720.webp" className="card-img-top" alt="immagine di placeholder" />
+                    <div className="card-body">
+                        <h2 className="card-title">Titolo del Post</h2>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptate quo eveniet sapiente tempora velit vitae animi odio repellendus voluptatibus, dolor ipsa inventore nostrum optio minus obcaecati, sequi distinctio officia?</p>
+                        <button className="btn btn-warning">Leggi di più</button>
+                        <button className="btn btn-danger">Elimina</button>
+                    </div>
+                </div>
+            </main>
+
+        </>
+
+
+    );
+}
+
+export default Main;
